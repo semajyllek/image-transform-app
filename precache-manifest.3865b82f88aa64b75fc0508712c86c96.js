135385015cec066b32e234835e5741e3147273de
semajyllek <mrkellyjam@gmail.com>
2025-04-06 08:10:13 -0700

    Updates

---
+++ precache-manifest.3865b82f88aa64b75fc0508712c86c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca874c417c7730d77f171e565971a70c",
+    "url": "/image-transform-app/index.html"
+  },
+  {
+    "revision": "70ba05bfaada8bd3b401",
+    "url": "/image-transform-app/static/css/main.85f62732.chunk.css"
+  },
+  {
+    "revision": "019c6b840697e994c50e",
+    "url": "/image-transform-app/static/js/2.28aca772.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/image-transform-app/static/js/2.28aca772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ba05bfaada8bd3b401",
+    "url": "/image-transform-app/static/js/main.3d12fa0b.chunk.js"
+  },
+  {
+    "revision": "e5ee9c289f9703dee455",
+    "url": "/image-transform-app/static/js/runtime-main.6f289f30.js"
+  }
+]);
